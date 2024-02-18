42578ff04363bd51f70ec4f6be40a0b0d54f6434
christopherkeibel <christopher.keibel@karakun.com>
2024-02-18 11:32:14 +0100

    add jinja template and generation config

--- code/model.py
+++ code/model.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn.functional as F
-from transformers import AutoModel, AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoModel, AutoTokenizer, AutoModelForCausalLM, GenerationConfig
 
 
 class EncoderModel:
@@ -9,7 +9,7 @@ class EncoderModel:
         self.model_id = model_id
         self.device = device if device else "cpu"
         self.tokenizer = AutoTokenizer.from_pretrained(self.model_id)
-        self.model = AutoModel.from_pretrained(self.model_id, device=self.device)
+        self.model = AutoModel.from_pretrained(self.model_id).to(self.device)
 
     def mean_pooling(self, model_output, attention_mask):
         token_embeddings = model_output[0] #First element of model_output contains all token embeddings
@@ -17,10 +17,11 @@ class EncoderModel:
         return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
 
     def tokenize(self, document):
-        return self.tokenizer(document, padding=True, truncation=True, return_tensors="pt").to(device)
+        return self.tokenizer(document, padding=True, truncation=True, return_tensors="pt").to(self.device)
 
     def model_inference(self, tokenized_document):
-        outputs = outputs = self.model(**tokenized_document)
+        with torch.no_grad():
+            outputs = outputs = self.model(**tokenized_document)
         return outputs
     
     def __call__(self, document):
@@ -32,24 +33,56 @@ class EncoderModel:
 class DecoderModel:
     """Causal language model to generate text based on a prompt."""
     def __init__(self, model_id, generation_config=None, device=None, **kwargs):
+        
+        assert isinstance(generation_config, GenerationConfig) or generation_config is None
         self.model_id = model_id
         self.device = device if device else "auto"
         self.model = AutoModelForCausalLM.from_pretrained(model_id, device_map=device, **kwargs)
         self.tokenizer = AutoTokenizer.from_pretrained(model_id)
-        # TODO: generation config
-        self.generation_config=generation_config if generation_config else {}
+        self.tokenizer.chat_template = self.get_jinja_tempalte()
+        self.generation_config=generation_config if generation_config else self.default_generation_config()
         # TODO: jinja template
+        
+    def get_jinja_tempalte(self):
+        rag_template = "{% for message in messages %}\n" \
+                            "{% if message['role'] == 'context' %}\n"\
+                                "{{ '<|context|>\n' + message['content'] + eos_token }}\n"\
+                            "{% elif message['role'] == 'system' %}\n"\
+                                "{{ '<|system|>\n' + message['content'] + eos_token }}\n"\
+                            "{% elif message['role'] == 'question' %}\n"\
+                                "{{ '<|question|>\n' + message['content'] + eos_token }}\n"\
+                            "{% elif message['role'] == 'assistant' %}\n"\
+                                "{{ '<|assistant|>\n'  + message['content'] + eos_token }}\n"\
+                            "{% endif %}\n"\
+                            "{% if loop.last and add_generation_prompt %}\n"\
+                                "{{ '<|assistant|>' }}\n"\
+                            "{% endif %}\n" \
+                        "{% endfor %}"
+        return rag_template
+        
+    def default_generation_config(self):
+        gen_cfg = GenerationConfig.from_pretrained(self.model_id)
+        gen_cfg.max_new_tokens = 50
+        gen_cfg.pad_token_id = self.tokenizer.pad_token_id
+        gen_cfg.begin_suppress_tokensrepetition_penalty = 5
+        gen_cfg.no_repeat_ngram_size = 3
+        return gen_cfg
 
     def tokenize(self, prompt):
         _device = "cuda" if self.device == "auto" else self.device
         tokenized_prompt = self.tokenizer(prompt, return_tensors="pt").to(_device)
         return tokenized_prompt
+    
+    def construct_rag_prompt(self, question, context):
+        messages = [
+            {"role": "system", "content": "Answer the question only based on the given context."},
+            {"role": "context", "content": context},
+            {"role": "question", "content": question}
+        ]
+        return self.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
 
-    def __call__(self, prompt):
+    def __call__(self, question, context):
+        prompt = self.construct_rag_prompt(question, context)
         tokenized_prompt = self.tokenize(prompt)
-        outputs = self.model.generate(**tokenized_prompt, max_new_tokens=50) # TODO: generation config
+        outputs = self.model.generate(**tokenized_prompt, generation_config=self.generation_config) # TODO: generation config
         return self.tokenizer.decode(outputs[0][len(tokenized_prompt.input_ids[0]):])
-
-    @staticmethod
-    def construct_rag_prompt(question, context):
-        pass
