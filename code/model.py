97f29958cfe3c8517f6b0e99a037d38571bdfc59
christopherkeibel <christopher.keibel@karakun.com>
2024-02-28 20:17:38 +0100

    document code

--- code/model.py
+++ code/model.py
@@ -13,6 +13,16 @@ nltk.download('wordnet')
 class EncoderModel:
     """Encoder class to encode documents and queries into a vector space."""
     def __init__(self, model_id, device=None):
+        """Initialize encoder.
+        
+        Args:
+            model_id: string to determine huggingface model
+            device: working device (cuda:i, cuda or cpu)
+                
+        Note:
+            If no device is defined, a fallback device is initialized. 
+            (in best case cuda)
+        """
         self.model_id = model_id
         fallback_device = "cuda" if torch.cuda.is_available() else device
         self.device = device if device else fallback_device
@@ -20,29 +30,74 @@ class EncoderModel:
         self.model = AutoModel.from_pretrained(self.model_id).to(self.device)
 
     def mean_pooling(self, model_output, attention_mask):
+        """Mean pooling over token embeddings to create sentence embeddings.
+        
+        Args:
+            model_output: token embeddings
+            attention_mask: attention_mask
+                
+        Returns:
+            sentence/ document embeddings
+        """
         token_embeddings = model_output[0] #First element of model_output contains all token embeddings
         input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
         return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
 
     def tokenize(self, document):
+        """Tokenize input string.
+        
+        Args:
+            document: string to be tokenized
+                
+        Returns:
+            tokenized string
+        """
         return self.tokenizer(document, padding=True, truncation=True, return_tensors="pt").to(self.device)
 
     def model_inference(self, tokenized_document):
+        """Create token embeddings.
+        
+        Args:
+            tokenized_document: tokenized document
+                
+        Returns:
+            token embeddings (one vector for each token)
+        """
         with torch.no_grad():
             outputs = outputs = self.model(**tokenized_document)
         return outputs
     
     def __call__(self, document):
+        """Function to create vector represention from input.
+        
+        Args:
+            document: string to be embedded
+                
+        Returns:
+            Normalized document embedding
+        """
+        # tokenize and embed tokens
         tokenized_document = self.tokenize(document)
         outputs = self.model_inference(tokenized_document)
+        # mean pooling over token embeddings
         embeddings = self.mean_pooling(outputs, tokenized_document["attention_mask"])
         torch.cuda.empty_cache()
         return F.normalize(embeddings, p=2, dim=1).cpu().numpy()
 
+    
 class DecoderModel:
     """Causal language model to generate text based on a prompt."""
     def __init__(self, model_id, generation_config=None, device=None, **kwargs):
+        """Initialize decoder.
         
+        Args:
+            model_id: string to determine huggingface model
+            generation_config: config to influence the generated output
+            device: working device (cuda:i, cuda or cpu)
+                
+        Note:
+            A jinja-template is set to structure the prompts appropriately for the task.
+        """
         assert isinstance(generation_config, GenerationConfig) or generation_config is None
         self.model_id = model_id
         self.device = device if device else "auto"
@@ -52,6 +107,12 @@ class DecoderModel:
         self.generation_config=generation_config if generation_config else self.default_generation_config()
         
     def get_jinja_tempalte(self):
+        """Jinja template to structure prompts.
+        
+        Returns:
+            A jinja-template in format of a string.
+        """
+        
         rag_template = "{% for message in messages %}\n" \
                             "{% if message['role'] == 'context' %}\n"\
                                 "{{ '<|context|>\n' + message['content'] + eos_token }}\n"\
@@ -69,6 +130,11 @@ class DecoderModel:
         return rag_template
         
     def default_generation_config(self):
+        """Default generation config.
+        
+        Returns:
+            A default generation config.
+        """
         gen_cfg = GenerationConfig.from_pretrained(self.model_id)
         gen_cfg.max_new_tokens = 150
         gen_cfg.pad_token_id = self.tokenizer.pad_token_id
@@ -77,34 +143,81 @@ class DecoderModel:
         return gen_cfg
 
     def tokenize(self, prompt):
+        """Tokenize input prompt.
+        
+        Args:
+            prompt: string prompt to be tokenized
+            
+        Returns:
+            Tokenized prompt.
+        """
         _device = "cuda" if self.device == "auto" else self.device
         tokenized_prompt = self.tokenizer(prompt, return_tensors="pt").to(_device)
         return tokenized_prompt
     
     def construct_rag_prompt(self, question, context):
+        """Structure prompt based on jinja-template.
+        
+        Args:
+            question: string question to be embedded in prompt
+            context: string context to be embedded in prompt
+            
+        Returns:
+            A structured instruction prompt as string.
+        """
         messages = [
-            {"role": "system", "content": "Answer the question only based on the given context."},
+            {"role": "system", "content": """Use the following pieces of context to answer the question at the end. 
+             If you can not answer the question from the given context, just reply with 'I do not know'. Do not try to make up an answer."""},
             {"role": "context", "content": context},
             {"role": "question", "content": question}
         ]
         return self.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
 
     def __call__(self, question, context):
+        """Function to generate string answer based on task input.
+        
+        Args:
+            question: string question to be embedded in prompt
+            context: string context to be embedded in prompt
+            
+        Returns:
+            Generated answer as string.
+        """
+        
+        # structure and tokenize prompt
         prompt = self.construct_rag_prompt(question, context)
         tokenized_prompt = self.tokenize(prompt)
-        outputs = self.model.generate(**tokenized_prompt, generation_config=self.generation_config) # TODO: generation config
+        # generate new tokens
+        outputs = self.model.generate(**tokenized_prompt, generation_config=self.generation_config)
+        # decode new tokens to string
         answer = self.tokenizer.decode(outputs[0][len(tokenized_prompt.input_ids[0]):])
         torch.cuda.empty_cache()
         return answer
+
     
 class BM25Model:
+    """BM25 model for sparse search."""
     def __init__(self):
-        self.mapping = dict()
+        """Initialized BM25 model.
+        
+        Note:
+            "documents" stores copurs to be searched on.
+            "bm25" points to bm25 model from "rank_bm25".
+            "lemmatizer" from nltk to clean documents.
+        """
         self.documents = list()
         self.bm25 = None
         self.lemmatizer = WordNetLemmatizer()
         
     def clean_string(self, text):
+        """Tokenizes input to words and lemmatizes words.
+        
+        Args:
+            text: string to clean
+            
+        Returns:
+            List of lemmatized words.
+        """
         result = []
         words = word_tokenize(text.lower())
         for word in words:
@@ -113,6 +226,14 @@ class BM25Model:
         return result
     
     def clean_documents(self, documents):
+        """Tokenize and lemmatize documents.
+        
+        Args:
+            documents: list of strings (documents)
+            
+        Returns:
+            A nested list of tokenized and lemmatized documents.
+        """
         cleaned_docs = []
         for doc in documents:
             doc = self.clean_string(doc)
@@ -121,10 +242,23 @@ class BM25Model:
         return cleaned_docs
     
     def add_documents(self, documents):
+        """Add cleaned documents to search corpus.
+        
+        Args:
+            documents: list of strings (documents)
+        """
         cleaned_docs = self.clean_documents(documents)
         self.bm25 = BM25Okapi(cleaned_docs)
         
     def search(self, query):
+        """Search tokenized and lemmatized corpus.
+        
+        Args:
+            query: search query in string format
+            
+        Returns:
+            A list of scores for all documents.
+        """
         cleaned_query = self.clean_string(query)
         scores = self.bm25.get_scores(cleaned_query)
         return scores
--- code/store.py
+++ code/store.py
@@ -6,7 +6,21 @@ from model import BM25Model, EncoderModel
 
 
 class VectorStore:
+    """VectorStore class that enables a hybrid search."""
     def __init__(self, model_id, hybrid=False, weight=0.5, distance_metric=None):
+        """Initialize VectorStore
+        
+        Args:
+            model_id: string describing huggingface model to use for retrieval
+            hybrid: boolean that determines the search mode (hybrid or dense)
+            weight: float between 0 and 1 that determines how much each search score should
+                influence the final result
+            distance_metric: string that determines how the dense search space should be used
+                (cosine, l2, ip)
+        
+        Note:
+            If hybrid is set to True, a BM25 model is instantiated.
+        """
         self.encoder = EncoderModel(model_id)
         self.bm25 = BM25Model() if hybrid else None
         self.documents = pd.DataFrame(columns=["id", "text", "vector"])
@@ -14,27 +28,74 @@ class VectorStore:
         self.weight = weight
         
     def normalize_scores(self, scores):
+        """Function to normalize search scores.
+        
+        Args:
+            scores: scores from a search result
+            
+        Returns:
+            Normalized scores
+        """
         return normalize([scores])[-1]
     
     def merge_results(self, dense_results, sparse_results):
+        """Function to merge search scores from hybrid search.
+        
+        Args:
+            dense_results: scores from dense search
+            sparse_results: scores from sparse search
+            
+        Returns:
+            Hybrid results if hybrid is set to True, else dense search scores
+        """
+        
+        # return dense dense scores if hybrid is False (therefore sparse_results is None)
         if sparse_results is None:
             return dense_results
+        
+        # get unique keys from both scores as they may not be identical
         merged_keys = set(list(dense_results.keys()) + list(sparse_results.keys()))
+        
+        # calculate hybrid scores
         hybrid_results = dict()
         for key in merged_keys:
+            # if a key is not found in a search result, set its score to 0
             dense_score =  0.0 if dense_results.get(key) is None else dense_results.get(key)
             sparse_score =  0.0 if sparse_results.get(key) is None else sparse_results.get(key)
+            # claculate and save hybrid score
             hybrid_score = (1 - self.weight) * sparse_score + self.weight * dense_score
             hybrid_results[key] = hybrid_score
         return hybrid_results
         
     def search(self, query, top_n=10):
+        """Function to search the vector store.
+        
+        Args:
+            query: string query
+            top_n: int to determine how much documents should be returned
+            
+        Returns:
+            A list of dictionaries structured as follows:
+                {
+                    score: float,
+                    document: string,
+                    id: int
+                }
+                
+        Note:
+            This function should be used to perform the search on the vector store.
+            It calls function for dense and sparse search.
+        """
+        # embed query and call dense search
         vectorized_query = self.encoder(query)
         dense_results = self.dense_search(list(vectorized_query), top_n)
         
+        # initialize sparse results to None, if hybrid is set to True, sparse search is called
         sparse_results = None
         if self.bm25:
             sparse_results = self.sparse_search(query, top_n)
+        
+        # merge both search results
         hybrid_results = self.merge_results(dense_results, sparse_results)
         result = [
             {
@@ -43,46 +104,103 @@ class VectorStore:
                 "id": self.documents.id.values[key]
             } for key, score in hybrid_results.items()
         ]
+        # return top_n results in descending order 
         return sorted(result, key=lambda x: x["score"], reverse=True)[:top_n]
     
     def sparse_search(self, query, top_n):
+        """Sparse search.
+        
+        Args:
+            query: string query
+            top_n: int to determine how much documents should be returned
+            
+        Returns:
+            A dictionary strucutred as follows:
+                {
+                    document id 1: search score 1,
+                    ...
+                    document id n: search score n,
+                }
+        """
+        
+        # call sparse search
         scores = self.bm25.search(query)
+        # extract ids of the top_n documents
         idxs = np.argpartition(scores, -top_n)[-top_n:]
         scores = scores[idxs]
+        # normalize scores
         scores = self.normalize_scores(scores)
         return {idx: score for idx, score in zip(idxs, scores)}       
     
     def dense_search(self, query, top_n):
+        """Dense search.
+        
+        Args:
+            query: string query
+            top_n: int to determine how much documents should be returned
+            
+        Returns:
+            A dictionary strucutred as follows:
+                {
+                    document id 1: search score 1,
+                    ...
+                    document id n: search score n,
+                }
+        """
         try:
             assert top_n <= self.documents.shape[0]
         except AssertionError:
             top_n = self.documents.shape[0]
-            
+        
+        # cast columns vectors to list 
+        # (otherwise type mismatch between query(list) and vectors (np.ndarray))
         vectors = self.documents.vector.values.tolist()
-            
+        
+        # calculate scores with specified distance (self.distance_metric)
         if self.distance_metric == "ip":
+            # extract ids of the top_n documents
             scores = np.inner(query, vectors)[-1]
             idxs = np.argpartition(scores, -top_n)[-top_n:]
         elif self.distance_metric == "l2":
+            # get absolute distances
             scores = abs(np.linalg.norm(np.array(query)-np.array(vectors), axis=1))
+            
+            # extract ids of the top_n documents
             if top_n == self.documents.shape[0]:
                 idxs = np.argpartition(scores, top_n-1)[:top_n]
             else:
                 idxs = np.argpartition(scores, top_n)[:top_n]
         else:
+            # extract ids of the top_n documents
             scores = cosine_similarity(query, vectors)[-1]
             idxs = np.argpartition(scores, -top_n)[-top_n:]
         
         scores = scores[idxs]
+        # normalize scores
         scores = self.normalize_scores(scores)
+        # substract distance scores from one
         if self.distance_metric == "l2":
             scores = np.subtract(1, scores)
         return {idx: score for idx, score in zip(idxs, scores)}
                 
     def add_documents(self, documents: list[str], ids=None):
+        """Add documents to vector store.
+        
+        Args:
+            documents: list of documents
+            ids: int document ids
+            
+        Note:
+            Creates word corpus for bm25 if hybrid is set to True.
+        """
+        
+        # embed documents into dense vector space
         vectors = self.encoder(documents)
+        # generate ids if not given
         if ids is None:
             ids = [i + len(self.documents) for i, _ in enumerate(documents)]
+        
+        # store new documents to pd.DataFrame
         new_data = pd.DataFrame(
                     {
                         "id": ids,
@@ -90,7 +208,9 @@ class VectorStore:
                         "vector": vectors.tolist()
                     }
                 )
+        # concat new_data and old documents
         self.documents = pd.concat([self.documents, new_data])
         
+        # generate word corpus for sparse search
         if self.bm25:
             self.bm25.add_documents(self.documents.text.values.tolist())
